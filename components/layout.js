3cd1f4af46b435244124c5633d0e0e04513da57d
Aurimas <aurimasvarnelis@gmail.com>
2022-05-22 21:15:09 +0300

    Removed not used stuff

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,21 @@
-import Header from "./header"
-//import Footer from "./footer"
 import Head from "next/head";
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Sidebar from "./sidebar";
+import Header from "./header";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+//import Footer from "./footer"
 
 export default function Layout({ children }) {
- 
-  return (
-    <>
-      <Head>
-        <title>CMIS</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Header />
-      {/* <Sidebar /> */}
-      <main>{children}</main>
-      {/* <Footer /> */}
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>CMIS</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Header />
+			{/* <Sidebar /> */}
+			<main>{children}</main>
+			{/* <Footer /> */}
+		</>
+	);
 }
